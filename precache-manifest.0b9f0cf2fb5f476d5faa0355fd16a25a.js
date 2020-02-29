4e51997f7b5dc956dcaf251447b15cf021300ea0
Reny10 <Reny10@users.noreply.github.com>
2020-02-29 14:45:57 +0000

    Updates

---
+++ precache-manifest.0b9f0cf2fb5f476d5faa0355fd16a25a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d751bc4c926e682c45a61efa44a59c62",
+    "url": "/facerecognitionbrain/index.html"
+  },
+  {
+    "revision": "154bb9067f9399f0166b",
+    "url": "/facerecognitionbrain/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "e3b212ce3c537049a994",
+    "url": "/facerecognitionbrain/static/css/main.045bff7e.chunk.css"
+  },
+  {
+    "revision": "154bb9067f9399f0166b",
+    "url": "/facerecognitionbrain/static/js/2.74ce663b.chunk.js"
+  },
+  {
+    "revision": "99bd0487192ec9e7d9ee8fbbd91ee444",
+    "url": "/facerecognitionbrain/static/js/2.74ce663b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e3b212ce3c537049a994",
+    "url": "/facerecognitionbrain/static/js/main.2a10c098.chunk.js"
+  },
+  {
+    "revision": "466a2f4fa9e4937c0bc5",
+    "url": "/facerecognitionbrain/static/js/runtime-main.a824e37d.js"
+  }
+]);
